dab5537db07f394646430fe57a3cc8eae79be1f4
Ataxaria <holyblade44@gmail.com>
2025-11-16 11:58:55 +0000

    Minor typo

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
-import CartItem from './CartItem';\
+import CartItem from './CartItem';
 import { addItem } from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
